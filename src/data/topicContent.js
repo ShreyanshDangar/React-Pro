d92e6731f1d7fd378b463a1168e17630f1c9971f
ShreyanshDangar <Shreyanshdangar.work@gmail.com>
2026-03-26 09:11:00 +0530

    feat: map lesson slugs to content modules

---
+++ src/data/topicContent.js
@@ -0,0 +1,61 @@
+import introduction from './content/introduction';
+import styling from './content/styling';
+import media from './content/media';
+import functions from './content/functions';
+import events from './content/events';
+import domManipulation from './content/domManipulation';
+import forms from './content/forms';
+import map from './content/map';
+import conditionalRendering from './content/conditionalRendering';
+import props from './content/props';
+import usestate from './content/usestate';
+import useeffect from './content/useeffect';
+import lifecycle from './content/lifecycle';
+import classComponents from './content/classComponents';
+import functionalComponents from './content/functionalComponents';
+import usecontext from './content/usecontext';
+import usememoUsecallback from './content/usememoUsecallback';
+import useref from './content/useref';
+import usereducer from './content/usereducer';
+import customHooks from './content/customHooks';
+import reactRouter from './content/reactRouter';
+import asyncApi from './content/asyncApi';
+import asyncExplained from './content/asyncExplained';
+import redux from './content/redux';
+import reduxToolkit from './content/reduxToolkit';
+import rtkQuery from './content/rtkQuery';
+import reduxApiVsUseeffect from './content/reduxApiVsUseeffect';
+import reduxVsStorage from './content/reduxVsStorage';
+
+const topicContent = {
+  introduction,
+  styling,
+  media,
+  functions,
+  events,
+  'dom-manipulation': domManipulation,
+  forms,
+  map,
+  'conditional-rendering': conditionalRendering,
+  props,
+  usestate,
+  useeffect,
+  lifecycle,
+  'class-components': classComponents,
+  'functional-components': functionalComponents,
+  usecontext,
+  'usememo-usecallback': usememoUsecallback,
+  useref,
+  usereducer,
+  'custom-hooks': customHooks,
+  'react-router': reactRouter,
+  'async-api': asyncApi,
+  'async-explained': asyncExplained,
+  redux,
+  'redux-toolkit': reduxToolkit,
+  'rtk-query': rtkQuery,
+  'redux-api-vs-useeffect': reduxApiVsUseeffect,
+  'redux-vs-storage': reduxVsStorage,
+};
+
+export default topicContent;
